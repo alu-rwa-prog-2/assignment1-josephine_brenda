d6ca9f45656dea90d8a9cb3e8d7e75a0ecef2783
Josephine-uwizeye <71031363+Josephine-uwizeye@users.noreply.github.com>
2021-01-17 15:13:40 +0200

    Update assignment1.java

--- assignment1.java
+++ assignment1.java
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
- author:josephine
+Author:josephine
+import java.util.Scanner;
 public class assignment1 {
 
 	public static void main(String[] args) {
@@ -14,46 +15,13 @@ public class assignment1 {
         Float width = UserInput.nextFloat();
 
         Float surface = length * width;
-        System.out.println("The area is:" + surface);
+        System.out.println("The area is:" + surface);//this will print the surface of rectangle
 
         Float perimeter = 2*(length+width);
-        System.out.println("The perimeter is: " + perimeter);
+        System.out.println("The perimeter is: " + perimeter);//this will print the perimeter
 
 
 	}
-
-}
-=======
-//Authors: Brenda & Josephine
-
-import java.util.Scanner;
-
-public class assignment1 {
-
-	public static void main(String[] args) {
-		Scanner UserInput = new Scanner(System.in);
-		
-		System.out.println("What is your name? ");
-        String name = UserInput.nextLine();
-        
-        System.out.println("What is your weight? ");
-        Float weight = UserInput.nextFloat();
-        
-        System.out.println("What is your height? ");
-	    Float height = UserInput.nextFloat();
-
-
-	    System.out.println("Name: " + name);
-	    System.out.println("Weight: " + weight);
-        System.out.println("Height: " + height);
-        
-        Float bmi = weight / (height * height);
-        System.out.println(name + " , your BMI is " + bmi);
-  
-	
-		
-	}
-
 }
 
 
